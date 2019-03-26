0d6238d48421a593356df4070e8de5a7099d5015
rubyszu <rubyonly233@gmail.com>
2019-03-26 10:36:02 +0800

    业务需求，key in dictionary也需要进行字符串替换

--- tavern/util/dict_util.py
+++ tavern/util/dict_util.py
@@ -85,7 +85,8 @@ def format_keys(val, variables):
         else:
             formatted = {}
             for key in val:
-                formatted[key] = format_keys(val[key], box_vars)
+                expect_key = format_keys(key, box_vars)
+                formatted[expect_key] = format_keys(val[key], box_vars)
     elif isinstance(val, (list, tuple)):
         formatted = [format_keys(item, box_vars) for item in val]
     elif isinstance(val, (ustr, str)):
